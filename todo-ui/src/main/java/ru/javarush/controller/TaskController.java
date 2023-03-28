157bc9aeb13e154a64f542e1605fddba0ae5def2
Anton Podkopaev <test@email.com>
2023-03-29 01:42:25 +0300

    add_config_to_ui_add_controller

--- todo-ui/src/main/java/ru/javarush/controller/TaskController.java
+++ todo-ui/src/main/java/ru/javarush/controller/TaskController.java
@@ -3,7 +3,12 @@ package ru.javarush.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.javarush.entity.Task;
 import ru.javarush.service.TaskService;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 @Controller
 @RequestMapping("/")
@@ -19,6 +24,8 @@ public class TaskController {
                         @RequestParam(value = "page",required = false,defaultValue = "1") int page,
                         @RequestParam(value = "limit", required = false,defaultValue = "1") int limit
     ){
+        List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
+        model.addAttribute("tasks", tasks);
         return "tasks";
     }
 
@@ -26,6 +33,21 @@ public class TaskController {
     public void edit(Model model,
                      @PathVariable Integer id,
                      @RequestBody TaskInfo info){
+        if (isNull(id) || id <= 0){
+            throw new RuntimeException("invalid id");
+        }
+        Task task = taskService.edit(id, info.getDescription(), info.getStatus());
+    }
 
+    @PostMapping("/")
+    public void add(Model model,
+                     @RequestBody TaskInfo info){
+        Task task = taskService.create(info.getDescription(), info.getStatus());
+    }
+    @DeleteMapping ("/")
+    public String delete(Model model,
+                    @PathVariable Integer id){
+        taskService.delete(id);
+        return "tasks";
     }
 }
