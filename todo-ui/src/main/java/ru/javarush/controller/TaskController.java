6924d39638f019613c63c0a55adf87bac08c49f5
Anton Podkopaev <test@email.com>
2023-03-29 22:09:44 +0300

    change_html_css_service_js

--- todo-ui/src/main/java/ru/javarush/controller/TaskController.java
+++ todo-ui/src/main/java/ru/javarush/controller/TaskController.java
@@ -9,6 +9,8 @@ import ru.javarush.entity.Task;
 import ru.javarush.service.TaskService;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Objects.isNull;
 
@@ -18,37 +20,48 @@ import static java.util.Objects.isNull;
 public class TaskController {
     private final TaskService taskService;
 
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String tasks(Model model,
                         @RequestParam(value = "page",required = false,defaultValue = "1") int page,
                         @RequestParam(value = "limit", required = false,defaultValue = "10") int limit
     ){
         List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
         model.addAttribute("tasks", tasks);
+        model.addAttribute("current_page", page);
+        int totalPages = (int) Math.ceil(1.0 * taskService.getAllCount() / limit);
+        if (totalPages > 1) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("page_numbers", pageNumbers);
+        }
         return "tasks";
     }
 
     @PostMapping("/{id}")
     public String edit(Model model,
-                     @PathVariable Integer id,
-                     @RequestBody TaskInfo info){
-        if (isNull(id) || id <= 0){
+                       @PathVariable Integer id,
+                       @RequestBody TaskInfo info) {
+        if (isNull(id) || id <= 0) {
             throw new RuntimeException("invalid id");
         }
+        System.out.println("start update");
         Task task = taskService.edit(id, info.getDescription(), info.getStatus());
-        return tasks(model,1,10);
+        return tasks(model, 1, 10);
     }
 
     @PostMapping("/")
     public String add(Model model,
-                     @RequestBody TaskInfo info){
+                      @RequestBody TaskInfo info) {
         Task task = taskService.create(info.getDescription(), info.getStatus());
-        return tasks(model,1,10);
+        return tasks(model, 1, 10);
     }
-    @DeleteMapping ("/")
+
+    @DeleteMapping("/{id}")
     public String delete(Model model,
-                    @PathVariable Integer id){
+                         @PathVariable Integer id) {
+        if (isNull(id) || id <= 0) {
+            throw new RuntimeException("invalid id");
+        }
         taskService.delete(id);
-        return tasks(model,1,10);
+        return tasks(model, 1, 10);
     }
 }
